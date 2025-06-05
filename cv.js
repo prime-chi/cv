b8aebb8e13a7eddc307932667695a30da31366be
prime-chi <113266576+prime-chi@users.noreply.github.com>
2025-06-05 01:09:16 +0100

    Add initial cv website files

---
+++ cv.js
@@ -0,0 +1,70 @@
+//console.log('Hello World!');
+//console.log('Welcome to variables');
+
+//let variableName = 'Prime Developer';
+/*
+const dev = 'prime';
+const name = `jude ${dev}`;
+console.log(name);
+
+const firstNumber = 45
+const secondnumber = 50
+
+const result = firstNumber + secondnumber
+console.log(result);
+
+
+
+const him = {
+    name: 'omahlay',
+    favSong: 'tell everybody ft tay iwar',
+    streams: 40000000, 
+}
+console.log(him);
+
+const date = new Date();
+console.log(typeof date);
+
+const a = 9;
+const b = 12;
+
+console.log(a >= b);
+console.log(a != b);
+console.log(b == a); //is equal to
+console.log(b === a); // strict equality 
+console.log(b !== a);
+
+
+
+
+const age = 20
+
+if (age <= 17){
+console.log('oh gee come do dorime!');
+} else {
+    console.log('gettouttttt!');
+}
+// */
+
+function sayHi(name){
+    console.log(`ke way, ${name}`);
+}
+
+sayHi('Mpa');
+
+function artist(name){
+    console.log(`He is, ${name}`);
+}
+artist('Omah lay');
+
+function add(a, b) {
+    return a + b;
+}
+const sum = add(6, 9);
+console.log(sum);
+
+const newStreams = (number) => {
+    return number * number * number;
+}
+const totalStreams = newStreams(300)
+console.log(totalStreams);
